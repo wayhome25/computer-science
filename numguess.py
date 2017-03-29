fb006a09d2e5f93f0e9afac56f5a6e54164e8754
siwabada <siwabada@gmail.com>
2017-03-29 16:17:05 +0900

    add 'too low', 'too high'

--- numguess.py
+++ numguess.py
@@ -5,4 +5,16 @@ answer = random.randint(1, 100)
 print(answer)
 
 username = input("what is you name?")
-print(username)
+
+while True:
+	guess = eval(input('hello %s Guess the number : ' % username))
+	if guess == answer:
+		print("Correct")
+		break
+	elif guess > answer:
+		print("Too High")
+	elif guess < answer:
+		print("Too low")
+	else:
+		print("You are wrong")
+		
