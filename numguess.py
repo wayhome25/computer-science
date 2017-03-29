ee22a650f865aa98abd00681ca14ffe2f0817a2a
siwabada <siwabada@gmail.com>
2017-03-29 16:32:35 +0900

    add chance == 3

--- numguess.py
+++ numguess.py
@@ -5,16 +5,18 @@ answer = random.randint(1, 100)
 print(answer)
 
 username = input("what is you name?")
-
+chance = 3
 while True:
 	guess = eval(input('hello %s Guess the number : ' % username))
 	if guess == answer:
 		print("Correct")
 		break
 	elif guess > answer:
-		print("Too High")
+		chance -= 1
+		print("Too High, (%d times left)" % (chance))
 	elif guess < answer:
-		print("Too low")
-	else:
-		print("You are wrong")
-		
+		chance -= 1
+		print("Too low, (%d times left)" % (chance))
+	if chance == 0:
+		print("The End")
+		break	
